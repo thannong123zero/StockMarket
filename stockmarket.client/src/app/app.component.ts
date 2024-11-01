3b125b9002880af6cbeedaedb6386f04e71d054d
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-11-01 22:49:31 +0700

    Update code

--- stockmarket.client/src/app/app.component.ts
+++ stockmarket.client/src/app/app.component.ts
@@ -14,6 +14,7 @@ import { iconSubset } from './icons/icon-subset';
   standalone: true,
   imports: [RouterOutlet]
 })
+
 export class AppComponent implements OnInit {
   title = 'CoreUI Angular Admin Template';
 
