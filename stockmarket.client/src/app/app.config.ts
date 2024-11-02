4baae0f81bd96c2c4d40816e6636abb300f54c09
Mr Zero <nguyenhoangtai2k@gmail.com>
2024-11-02 12:00:16 +0700

    Update code

--- stockmarket.client/src/app/app.config.ts
+++ stockmarket.client/src/app/app.config.ts
@@ -1,34 +1,42 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-  withHashLocation,
-  withInMemoryScrolling,
-  withRouterConfig,
-  withViewTransitions
-} from '@angular/router';
+// import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+// import { provideAnimations } from '@angular/platform-browser/animations';
+// import {
+//   provideRouter,
+//   withEnabledBlockingInitialNavigation,
+//   withHashLocation,
+//   withInMemoryScrolling,
+//   withRouterConfig,
+//   withViewTransitions
+// } from '@angular/router';
 
-import { DropdownModule, SidebarModule } from '@coreui/angular';
-import { IconSetService } from '@coreui/icons-angular';
+// import { DropdownModule, SidebarModule } from '@coreui/angular';
+// import { IconSetService } from '@coreui/icons-angular';
+// import { routes } from './app.routes';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideRouter(routes,
+//       withRouterConfig({
+//         onSameUrlNavigation: 'reload'
+//       }),
+//       withInMemoryScrolling({
+//         scrollPositionRestoration: 'top',
+//         anchorScrolling: 'enabled'
+//       }),
+//       withEnabledBlockingInitialNavigation(),
+//       withViewTransitions(),
+//       withHashLocation()
+//     ),
+//     importProvidersFrom(SidebarModule, DropdownModule),
+//     IconSetService,
+//     provideAnimations()
+//   ]
+// };
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes,
-      withRouterConfig({
-        onSameUrlNavigation: 'reload'
-      }),
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'top',
-        anchorScrolling: 'enabled'
-      }),
-      withEnabledBlockingInitialNavigation(),
-      withViewTransitions(),
-      withHashLocation()
-    ),
-    importProvidersFrom(SidebarModule, DropdownModule),
-    IconSetService,
-    provideAnimations()
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
